7baa32b03cbcd00ec092cecc142896b8baa5c523
Christopher Plain <me@christopherplain.com>
2026-04-17 21:10:48 -0700

    chore: update lnk to v0.6.0

--- Formula/lnk.rb
+++ Formula/lnk.rb
@@ -1,8 +1,8 @@
 class Lnk < Formula
   desc "Opinionated symlink manager for dotfiles and more"
   homepage "https://github.com/cpplain/lnk"
-  url "https://github.com/cpplain/lnk/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "55d9fa5b454395b76dcff3554b6ad48a8bd8ea9a75331cb9b68ba5e3f56754db"
+  url "https://github.com/cpplain/lnk/archive/refs/tags/v0.6.0.tar.gz"
+  sha256 "68b0de5f1a6f70c626ea1b11434722c352020662886c4b28e2101b61a6b1dac5"
   license "MIT"
 
   depends_on "go" => :build
@@ -12,7 +12,7 @@ class Lnk < Formula
       -s -w
       -X main.version=#{version}
     ]
-    system "go", "build", *std_go_args(ldflags:), "./cmd/lnk"
+    system "go", "build", *std_go_args(ldflags:), "."
   end
 
   test do
